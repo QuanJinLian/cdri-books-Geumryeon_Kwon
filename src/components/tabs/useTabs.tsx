f024a5f843266276f509dd078d0e91f4139ac565
권금련 <micka772@naver.com>
2026-02-24 12:47:10 +0900

    mui Tab 컴포넌트 랩핑 작업

---
+++ src/components/tabs/useTabs.tsx
@@ -0,0 +1,67 @@
+import { Box, Tab } from "@mui/material";
+import { ReactNode, SyntheticEvent, useState } from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+
+export type TabItem = {
+  id: string; //value
+  label: string;
+  component: ReactNode;
+  tabProps?: Parameters<typeof Tab>[0];
+  tabPanelProps?: Parameters<typeof TabPanel>[0];
+};
+
+export type UseTabsProps = {
+  tabs: TabItem[];
+  selectedTab?: UseTabsProps["tabs"][number]["id"];
+  onChange?: (event: SyntheticEvent, newValue: string) => void;
+  tabListBoxProps?: Parameters<typeof Box>[0];
+  tabListProps?: Parameters<typeof TabList>[0];
+};
+
+export function useTabs({
+  tabs,
+  selectedTab,
+  tabListBoxProps,
+  tabListProps,
+  onChange,
+}: UseTabsProps) {
+  const [selectedId, setSelectedId] = useState(selectedTab || "");
+
+  const handleChange = (e: SyntheticEvent, newValue: string) => {
+    onChange?.(e, newValue);
+    setSelectedId(newValue);
+  };
+  const Tabs = () => {
+    return (
+      <TabContext value={selectedId}>
+        <Box {...(tabListBoxProps || {})}>
+          <TabList
+            {...(tabListProps || {})}
+            sx={{
+              "& .MuiTabs-indicator": {
+                height: "1px", // 선택 사항 border 얇게 만들기
+                backgroundColor: "var(--primary)",
+              },
+              ...(tabListProps?.sx || {}),
+            }}
+            onChange={handleChange}
+          >
+            {tabs?.map(({ label, id, tabProps }) => (
+              <Tab key={id} {...tabProps} label={label} value={id} />
+            ))}
+          </TabList>
+        </Box>
+        {tabs?.map(({ component, id, tabPanelProps }) => (
+          <TabPanel key={id} {...tabPanelProps} value={id}>
+            {component}
+          </TabPanel>
+        ))}
+      </TabContext>
+    );
+  };
+
+  return {
+    Tabs,
+    selectedId,
+  };
+}
